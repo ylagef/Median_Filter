2425a5ea0002c52bb7f7bfdf1ca373c629b05e2a
Yeray Lage <ylagef@gmail.com>
2018-02-27 20:47:01 +0100

    Working hard.

--- src/MyProblem.java
+++ src/MyProblem.java
@@ -6,33 +6,26 @@ import java.util.Random;
 public class MyProblem {
     public static HashMap<Integer, Output> outputs = new HashMap<>(); //Global HashMap with the Output objects.
     public static MyMatrix matrix;
+    public static double[][] convertedMatrix;
+    static int numThreads;
+    public static List<Thread> threadList = new ArrayList<>(numThreads); //ArrayList for the created Threads.
 
     public static void main(String[] args) {
-        int numThreads = Integer.parseInt(args[0]);
+        numThreads = Integer.parseInt(args[0]);
 
-        matrix = new MyMatrix(5, 6); //Generates new matrix
-        matrix.constantFill(2);
+        matrix = new MyMatrix(3, 3); //Generates new matrix
+        convertedMatrix = new double[3][3]; //Reserves space
+        matrix.constantFill(2); //Fill matrix
         System.out.println(matrix.toString());
-        double[][] converted = matrix.convertMatrix();
-        for (double[] d : converted) {
+
+
+        //Do the median filter
+        matrix.medianFilter();
+        for (double[] d : convertedMatrix) {
             for (Double d1 : d) {
                 System.out.print(d1 + " \t");
             }
             System.out.println();
-        }
-
-        List<Thread> threadList = new ArrayList<>(numThreads); //ArrayList for the created Threads.
-
-        //Create and start Threads. As much as selected by the argument value.
-        for (int i = 0; i < numThreads; ++i) {
-            threadList.add(i, new Thread(new MyThread(), Integer.toString(i)));
-            threadList.get(i).start();
-        }
-
-        //Interrupt threads after main sleep
-        for (Thread t : threadList) {
-            t.interrupt();
-            MyProblem.outputs.put(Integer.parseInt(t.getName()), new Output(System.currentTimeMillis()));
         }
 
         //Check threads end
@@ -52,12 +45,30 @@ public class MyProblem {
 }
 
 class MyThread implements Runnable {
-    private ThreadLocal<Double> pi = ThreadLocal.withInitial(() -> 0.0); //Setting up the initial value
+    private ThreadLocal<Cell> startCell = new ThreadLocal<>();
+    private ThreadLocal<Cell> endCell = new ThreadLocal<>();
+
+    public MyThread(Cell startCell, Cell endCell) {
+        this.startCell.set(startCell);
+        this.endCell.set(endCell);
+    }
 
     //Code going to be executed by the thread.
     @Override
     public void run() {
+        double[][] auxMatrix = new double[MyProblem.matrix.getRowsSize()][MyProblem.matrix.getColsSize()]; // Create auxiliary matrix
+        for (int r = startCell.get().getR(); r < endCell.get().getR(); r++) {
+            for (int c = startCell.get().getC(); c < endCell.get().getC(); c++) {
+                double mediumValue;
 
+                Cell auxCell = new Cell(r, c);
+
+                mediumValue = auxCell.getMedium();
+
+                auxMatrix[r][c] = mediumValue;
+            }
+        }
+        MyProblem.convertedMatrix = auxMatrix;
     }
 }
 
@@ -107,12 +118,43 @@ class MyMatrix {
         }
     }
 
-    public double[][] convertMatrix() {
+    public double[][] medianFilter() {
         System.out.println("---------------------\n");
+
+        int cellsQuantity = this.getRowsSize() * this.getColsSize();
+        int cellsPerThread = cellsQuantity / MyProblem.numThreads;
+
+        int sr = 0, sc = 0, er, ec;
+        int id = 0;
+        for (int assigned = 0; assigned < cellsQuantity; assigned += cellsPerThread) {
+            er = (assigned + cellsPerThread) % this.getColsSize();
+            ec = assigned + (cellsPerThread - (er * this.getColsSize()))-1;
+
+            MyProblem.threadList.add(id, new Thread(new MyThread(new Cell(sr, sc), new Cell(er, ec)), Integer.toString(id)));
+            MyProblem.threadList.get(id++).start();
+
+            if (ec != this.getColsSize()-1) {
+                sr = er;
+                sc = ec + 1;
+            } else {
+                sr = er + 1;
+                sc = 0;
+            }
+        }
+
+        //Create and start Threads. As much as selected by the argument value.
+        for (int i = 0; i < MyProblem.numThreads; ++i) {
+
+            MyProblem.threadList.add(i, new Thread(new MyThread(new Cell(1, 2), new Cell(1, 2)), Integer.toString(i)));
+            MyProblem.threadList.get(i).start();
+        }
+
+
         double[][] auxMatrix = new double[this.getRowsSize()][this.getColsSize()]; // Create auxiliary matrix
         for (int r = 0; r < this.getRowsSize(); r++) {
             for (int c = 0; c < this.getColsSize(); c++) {
                 double mediumValue;
+
                 Cell auxCell = new Cell(r, c);
 
                 mediumValue = auxCell.getMedium();
@@ -165,7 +207,8 @@ class Cell {
         int actual = MyProblem.matrix.getValue(r, c);
 
         int top, bottom, left, right, topLeft, topRight, bottomLeft, bottomRight;
-        
+
+        // I KNOW that this way is not optimal. Otherwise it was a secure way for checking all chances.
         if (r == 0) {//First row
             bottom = MyProblem.matrix.getValue(r + 1, c);
             top = MyProblem.matrix.getValue(r + 1, c);
@@ -253,5 +296,13 @@ class Cell {
         medium = Math.floor(medium * 100) / 100; //For having just 2 decimals
 
         return medium;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public int getC() {
+        return c;
     }
 }
